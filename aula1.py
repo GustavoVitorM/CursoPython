b7b463bbb7151c25f3ffc7493a6edd94b1c6ad03
GustavoVitorM <gustavovmps@gmail.com>
2024-05-18 15:22:24 -0300

    Primeiras aulas

---
+++ aula1.py
@@ -0,0 +1,9 @@
+# Primeira aula do Curso de Python - Básico
+
+# Comentário Símples - 
+
+'''
+DocStrngs - NÃO É UM COMENTÁRIO
+É possível Escrever em mais de uma linha,
+o que está aqui sera lido mas não executado
+'''
---
+++ aula3.py
@@ -0,0 +1,23 @@
+# Tipos de Dados - STR
+
+'''
+    Python = Linguagem de programação,
+    Tipo de Tipagem = Dinamica / Forte
+        Significa que o python já sabe o tipo do dado passado
+    str --> String --> Texto
+    Strings são textos dentro de aspas
+'''
+
+# Aspas Simples
+print('Gustavo Vitor')
+print('Gustavo "Vitor"')
+
+# Aspas Duplas
+print("Gustavo Vitor")
+print("Gustavo 'Vitor'")
+
+# Escape
+print("Gustavo \"Vitor\"")
+
+# r
+print(r"Gustavo \"Vitor\"")
