cdf34b177703137369bdc593d231a7f459854e38
GustavoVitorM <gustavovmps@gmail.com>
2024-05-26 10:59:08 -0300

    arquivos deleted

--- aula1.py
+++
@@ -1,9 +0,0 @@
-# Primeira aula do Curso de Python - Básico
-
-# Comentário Símples - 
-
-'''
-DocStrngs - NÃO É UM COMENTÁRIO
-É possível Escrever em mais de uma linha,
-o que está aqui sera lido mas não executado
-'''
