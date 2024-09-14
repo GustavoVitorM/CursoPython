d6d48680087eba30a10a458cbd1746a7daac47f2
GustavoVitorM <gustavovmps@gmail.com>
2024-09-14 13:40:27 -0300

    Atualizaçoes

--- aulas/aulax.py
+++ aulas/aulax.py
@@ -1,3 +1,8 @@
-nomes = ["Maria", "Gustavo", "Rosa"]
-nome1, *resto = nomes
-print(nome1, resto)
+nome = "Gustavo Vitor"
+altura = 1.80
+peso = 80
+imc = peso / (altura * altura)
+
+print(f"{nome} tem {altura} de altura, \n",
+      f"pesa {peso} quilos e seu IMC é \n",
+      f"{imc}.")
