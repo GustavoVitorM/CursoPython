b10ce4870ca73316385be2e0638215e5be3fd9e5
GustavoVitorM <gustavovmps@gmail.com>
2024-09-14 19:23:45 -0300

    Ajustando git

---
+++ aulas/alua108.py
@@ -0,0 +1,18 @@
+c =1323
+
+def escopo ():
+    c = 1
+
+    print(c)
+    def escopo2():
+        global c
+        c = 3
+        print(c)
+    
+    escopo2()
+    print(c)
+
+
+print(c)
+escopo()
+print(c)
--- aulas/aulax.py
+++
@@ -1,8 +0,0 @@
-nome = "Gustavo Vitor"
-altura = 1.80
-peso = 80
-imc = peso / (altura * altura)
-
-print(f"{nome} tem {altura} de altura, \n",
-      f"pesa {peso} quilos e seu IMC é \n",
-      f"{imc}.")
