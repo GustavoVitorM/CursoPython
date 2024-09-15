be7e142b84000777bde8bb9780f699aa8fbd6aee
Gustavo Vitor <gustavovmps@gmail.com>
2024-09-14 21:48:51 -0300

    Atualização dia 1.2 Funções + Closure

---
+++ desafios/des14.py
@@ -0,0 +1,13 @@
+
+def equ(mult):
+    def multi(value):
+        return value * mult
+    return multi
+
+duplicando = equ(2)
+triplicando = equ(3)
+quadruplicando = equ(4)
+
+print(duplicando(2))
+print(triplicando(2))
+print(quadruplicando(2))
